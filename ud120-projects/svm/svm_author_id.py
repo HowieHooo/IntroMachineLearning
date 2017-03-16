400c4c01807f2dc1c9510e956084c45f42bdcb56
HowieHua <howiehua.hh@gmail.com>
2017-03-16 16:00:42 -0700

    modify the svm algorithm

--- ud120-projects/svm/svm_author_id.py
+++ ud120-projects/svm/svm_author_id.py
@@ -24,7 +24,19 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+t0 = time()
+clf = SVC(kernel='rbf', C=10000)
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+print clf.score(features_test, labels_test)
+print "testing time:", round(time()-t0, 3), "s"
 
 #########################################################
+
 
 
